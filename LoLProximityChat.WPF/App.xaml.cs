db33dddf75bf079d37ee9ebb2f7290d0f31ed9df
Thomas Boisson <thomas.boisson@epita.fr>
2026-03-22 23:10:39 +0100

    chore: public ip

--- LoLProximityChat.WPF/App.xaml.cs
+++ LoLProximityChat.WPF/App.xaml.cs
@@ -7,4 +7,11 @@ namespace LoLProximityChat.WPF;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
-public partial class App : System.Windows.Application { }
+public partial class App : System.Windows.Application
+{
+    protected override void OnExit(ExitEventArgs e)
+    {
+        base.OnExit(e);
+        Environment.Exit(0); // Force kill de tous les threads
+    }
+}
