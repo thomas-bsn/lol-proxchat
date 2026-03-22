1fc1f514000501c305edd9cc7266577d4ced1265
Thomas Boisson <thomas.boisson@epita.fr>
2026-03-22 22:15:51 +0100

    m

--- LoLProximityChat.Server/Program.cs
+++ LoLProximityChat.Server/Program.cs
@@ -2,7 +2,9 @@ using LoLProximityChat.Server.Hubs;
 using LoLProximityChat.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5128";
 
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<RoomService>();
 
