a74b1ff279526da8addca70489d9f456968a08df
Thomas BOISSON <thomas.boisson@epita.fr>
2026-03-26 18:21:27 +0100

    deleted redundant file

--- LoLProximityChat.Core/Models/VolumeMapping.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LoLProximityChat.Core.Models
-{
-    public class VolumeMapping
-    {
-        public string PlayerId      { get; set; } = "";
-        public float  VolumePercent { get; set; }
-    }
-}
