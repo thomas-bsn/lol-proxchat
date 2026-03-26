025da75dc89a07aa151fbe2867d674f3701f8708
Thomas BOISSON <thomas.boisson@epita.fr>
2026-03-26 18:16:57 +0100

    m

--- LoLProximityChat.Core/Interfaces/IRoomService.cs
+++ LoLProximityChat.Core/Interfaces/IRoomService.cs
@@ -2,6 +2,6 @@ namespace LoLProximityChat.Core.Interfaces
 {
     public interface IRoomService
     {
-        Task<string> JoinOrCreateAsync(string roomId, string playerId, CancellationToken ct);
+        Task<string?> JoinOrCreateAsync(string roomId, string playerId, CancellationToken ct);
     }
 }
